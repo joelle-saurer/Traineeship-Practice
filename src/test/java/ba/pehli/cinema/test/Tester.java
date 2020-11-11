85d68992d1f9920ccc22b1673b5d5b1d7e0a3609
joelle-saurer <72863109+joelle-saurer@users.noreply.github.com>
2020-11-11 13:23:05 +0100

    Update Tester.java

--- src/test/java/ba/pehli/cinema/test/Tester.java
+++ src/test/java/ba/pehli/cinema/test/Tester.java
@@ -11,13 +11,6 @@ import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.Twitter;
-This is merge content!
-Hello there
-<<<<<<< HEAD
-This is on the newBranch
-=======
-This is on the second branch
->>>>>>> SecondBranch
 public class Tester {
 	
 
